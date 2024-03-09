89aabad38b6e55d8cb940989890af4266fc85f9c
sivek <127289850+s1vek@users.noreply.github.com>
2024-03-09 14:10:53 +0100

    Add files via upload

---
+++ Project/Project/IRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project
+{
+    public interface IRepository<T> where T : IBaseClass
+    {
+        T GetByID(int id);
+        IEnumerable<T> GetAll();
+        void Insert(T element);
+        void Update(T element);
+        void Delete(T element);
+    }
+}
